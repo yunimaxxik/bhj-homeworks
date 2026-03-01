25c91c92954e144fad344099419d22efb5b83607
yunimaxxik <nicxefonn@yandex.ru>
2026-03-01 17:03:33 +0300

    completed mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,31 @@
+const holeElements = document.querySelectorAll('.hole');
+const deadElement = document.getElementById('dead');
+const lostElement = document.getElementById('lost');
+let countOfDeads = 0;
+let countOfLosts = 0;
+
+holeElements.forEach((item) => {
+	item.addEventListener('click', () => {
+		if (item.classList.contains('hole_has-mole')) {
+			countOfDeads++;
+			deadElement.textContent = countOfDeads;
+			if (countOfDeads === 10) {
+				alert('Победа');
+				countOfDeads = 0;
+				countOfLosts = 0;
+				deadElement.textContent = 0;
+				lostElement.textContent = 0;
+			}
+		} else {
+			countOfLosts++;
+			lostElement.textContent = countOfLosts;
+			if (countOfLosts === 5) {
+				alert('Поражение');
+				countOfDeads = 0;
+				countOfLosts = 0;
+				deadElement.textContent = 0;
+				lostElement.textContent = 0;
+			}
+		}
+	});
+});
