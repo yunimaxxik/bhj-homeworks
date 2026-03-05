90465830e7a8b8593e0e237a6e271bdf7ec8f801
yunimaxxik <nicxefonn@yandex.ru>
2026-03-05 18:33:03 +0300

    repair mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,27 +4,31 @@ const lostElement = document.getElementById('lost');
 let countOfDeads = 0;
 let countOfLosts = 0;
 
+function finishGame(result) {
+	if (result) {
+		alert('Победа');
+	} else {
+		alert('Поражение');
+	}
+	countOfDeads = 0;
+	countOfLosts = 0;
+	deadElement.textContent = 0;
+	lostElement.textContent = 0;
+}
+
 holeElements.forEach((item) => {
 	item.addEventListener('click', () => {
 		if (item.classList.contains('hole_has-mole')) {
 			countOfDeads++;
 			deadElement.textContent = countOfDeads;
 			if (countOfDeads === 10) {
-				alert('Победа');
-				countOfDeads = 0;
-				countOfLosts = 0;
-				deadElement.textContent = 0;
-				lostElement.textContent = 0;
+				finishGame(true);
 			}
 		} else {
 			countOfLosts++;
 			lostElement.textContent = countOfLosts;
 			if (countOfLosts === 5) {
-				alert('Поражение');
-				countOfDeads = 0;
-				countOfLosts = 0;
-				deadElement.textContent = 0;
-				lostElement.textContent = 0;
+				finishGame(false);
 			}
 		}
 	});
