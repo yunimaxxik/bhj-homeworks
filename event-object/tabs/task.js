1caadb033de2a300e56e1f57c7dc5bf542281793
yunimaxxik <nicxefonn@yandex.ru>
2026-03-09 21:01:42 +0300

    tabs repaired

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,15 +1,19 @@
-const tabElements = document.querySelectorAll('.tab');
-const tabContentElements = document.querySelectorAll('.tab__content');
+const tabs = document.querySelectorAll('.tabs');
 
-tabElements.forEach((item, index) => {
-	item.addEventListener('click', () => {
-		for (let i = 0; i < tabElements.length; i++) {
-			if (i !== index) {
-				tabElements[i].classList.remove('tab_active');
-				tabContentElements[i].classList.remove('tab__content_active');
+tabs.forEach((tab) => {
+	const tabElements = tab.querySelectorAll('.tab');
+	const tabContentElements = tab.querySelectorAll('.tab__content');
+
+	tabElements.forEach((item, index) => {
+		item.addEventListener('click', () => {
+			for (let i = 0; i < tabElements.length; i++) {
+				if (i !== index) {
+					tabElements[i].classList.remove('tab_active');
+					tabContentElements[i].classList.remove('tab__content_active');
+				}
 			}
-		}
-		item.classList.add('tab_active');
-		tabContentElements[index].classList.add('tab__content_active');
+			item.classList.add('tab_active');
+			tabContentElements[index].classList.add('tab__content_active');
+		});
 	});
 });
